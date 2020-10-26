5690cdb01faa0db12c65f45df64b72d50c093172
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-26 15:11:02 +0100

    Update editLecture.view.php

--- app/views/editLecture.view.php
+++ app/views/editLecture.view.php
@@ -1,51 +1,55 @@
 <?php require 'partials/header.php';  ?>
 
-  <div class="container-fluid"> 
-        <div class="row p-2" style='background-color:#4f4d49';> 
-            <div> 
-               <span class="text-white">uredjivanje lekcije </span>
-            </div> 
+<div class="under_nav">
 
-         </div>
+    <p class="under_nav_parag">Uredi lekciju</p>
 
-         <?php if(isset($_SESSION['errors'])){
-        foreach($_SESSION['errors'] as $er){
-            ?> 
-            <div class="alert alert-danger text-center" role="alert">
-                 <?php echo $er;?>
-            </div>
-            <?php
-        }
-        unset($_SESSION['errors']);       
-    } ?>
+</div>
 
-         <form action="editLec_post?edit_id=<?php echo $_GET['id']; ?>" method="post"  enctype="multipart/form-data">
-            <div class="form-group">
-                <p class="mt-3"><b>naslov lekcije</b></p>
-                <input type="text" value="<?php echo $lecture->name;?>"  name="name" class="form-control col-xl-4 col-6" id="exampleFormControlInput1">
-             </div>
-            <div class="form-group">
-                <p class="mt-3"><b>  kratak opis  </b></p>
-                <textarea class="form-control col-xl-4 col-8" name="short_description" rows="3"><?php echo $lecture->short_description;?> </textarea>
-            </div>
 
-            <div class="form-group">
-                 <p class="mt-3"><b>  sadrzaj  </b></p>
-                 <textarea class="form-control col-xl-6 col-10" name="content"   rows="7"><?php echo $lecture->content; ?> </textarea>
-            </div>
+<div class="std_exercise_txt">
 
-             <div class="form-group">
-                 <p class="mt-3"><b>izaberi fajl  </b></p>
-                  <input type="file" value="<?php echo $lecture->document;?>" class="form-control-file" name="document" id="exampleFormControlFile1">
-            </div>
+    <div class="std_exe_txt"></div>
+    <div class="std_exe_txt">
 
-            <button type="submit" class="btn btn-primary col-xl-2 col-4 btn-block">Uredi</button>
+        <?php if(isset($_SESSION['errors'])){
+            foreach($_SESSION['errors'] as $er){
+                ?>
+                <div class="flash_message">
+                    <?php echo $er;?>
+                </div>
+                <?php
+            }
+            unset($_SESSION['errors']);
+        } ?>
 
 
 
-         </form>
+        <form action="editLec_post?edit_id=<?php echo $_GET['id']; ?>" method="post"  enctype="multipart/form-data">
 
-     </div>
+            <p class="form_parag">Naziv lekcije</p>
+            <input class="input_lec" value="<?php echo $lecture->name;?>" type="text" name="name">
+
+            <p class="form_parag">Sadrzaj lekcije</p>
+            <textarea name="content" class="textarea_std" rows="12" cols="100"><?php echo $lecture->content; ?></textarea>
+          
+
+
+
+            <button type="submit" class="deliver_exercise_button">Uredi</button>
+
+        </form>
+
+
+
+
+    </div>
+
+    <div class="std_exe_txt"></div>
+
+
+
+</div>
 
 </body>
 </html>
