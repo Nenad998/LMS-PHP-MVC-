cda82f1f9e54b27c04bac97b126ecfaca007a0e9
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-26 15:07:39 +0100

    Update lectures.view.php

--- app/views/lectures.view.php
+++ app/views/lectures.view.php
@@ -1,34 +1,35 @@
 <?php require 'partials/header.php';  ?>
 
-  <div class="container-fluid mt-2  h-75 d-inline-block ">
-    <div class="row "> 
-        <div class="col-sm-12 col-xl-12  text-center p-2" style='background-color:#4f4d49';>
-            <div class="align-middle"> <h4 class="text-white"><?php echo $lecture->name;?></h4> </div> 
-         </div>
-          <div class="col-xl-2"> </div>
-          <div class="col-xl-8 mt-3"> 
-            <p class="text-justify"><?php echo $lecture->content; ?> </p>
-            <table class="table col-xl-12  text-center mt-3">
-              <tbody>
-              <?php foreach($lecture->exe as $lect){ ?> 
-                <tr>
+<div class="under_nav">
 
-                  <td><b><?php echo $lect->name; ?> </b></td>
-                  <td><a id="pogl_zad" href="exercise?exe_id=<?php echo $lect->id; ?>"><button class="btn btn-primary btn-block ">Pogledaj zadatak</button></a></td>
-                  <td><a id="pogl_zad" href="editExercise?id=<?php echo $lect->id; ?>"><button class="btn btn-danger btn-block ">Izmeni</button> </a></td>
-                  <td><a id="pogl_zad" href="deleteExercise?id=<?php echo $lect->id; ?>&eid=<?php echo $lecture->id;?>"><button class="btn btn-warning btn-block ">Obrisi</button> </a></td>
-                  <td><a id="pogl_zad" href="completed_task?id=<?php echo $lect->id;  ?>"><button class="btn btn-success btn-block ">Uradjen zadatak</button> </a></td>
-                  
+    <p class="under_nav_parag"><?php echo  $lecture->name;  ?></p>
 
-                </tr>
-                <?php } ?>
-               </tbody>
-             </table>
-             <a href="addExercise?add_id=<?php echo $lecture->id;  ?>" class="btn btn-primary mt-2 float-xl-right float-sm-right" role="button" aria-pressed="true">dodaj zadatak</a>
-           </div>
-           <div class="col-xl-2">  </div>
-     </div>
-   </div>
+</div>
+
+<div class="std_lecture_txt">
+
+    <div class="std_item_txt"></div>
+    <div class="std_item_txt"><p><?php echo $lecture->content;?></p></div>
+    <div class="std_item_txt"></div>
+
+
+
+</div>
+
+<a href="addExercise?add_id=<?php echo $lecture->id;  ?>"> <button class="buy_course_button">Dodaj zadatak</button> </a>
+
+<div class="std_lecture_teacher">
+    <?php foreach($lecture->exe as $lect){ ?>
+        <div class="std_item" id="name_exe"><a href="exercise?exe_id=<?php echo $lect->id; ?>"><?php echo $lect->name; ?></a></div>
+        <div class="std_item" id="edit"><a href="editExercise?id=<?php echo $lect->id; ?>">Izmeni</a></div>
+        <div class="std_item" id="delete"><a href="deleteExercise?id=<?php echo $lect->id; ?>&eid=<?php echo $lecture->id;?>">Obrisi</a></div>
+        <div class="std_item" id="done"><a href="completed_task?id=<?php echo $lect->id;  ?>">Uradjen zadatak</a></div>
+
+    <?php } ?>
+</div>
+
+
+
 
 
 </body>
