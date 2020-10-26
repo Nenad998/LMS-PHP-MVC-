23aa9be62c81e6632f67e6fc00fe933614171949
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-26 15:14:25 +0100

    Update adminPanel.view.php

--- app/views/adminPanel.view.php
+++ app/views/adminPanel.view.php
@@ -1,45 +1,27 @@
 <?php require 'partials/header.php';  ?>
 
 
-  <div class="container-fluid mt-2  h-75 d-inline-block ">
-    <div class="row "> 
-       <div class="col-sm-12 col-xl-12  text-center p-2" style='background-color:#4f4d49';>
-          <div class="align-middle "> <h4 class="text-white">Admin panel</h4>    </div> 
-        </div>
-        <a href="addCourses" class="btn btn-primary mt-2" role="button" aria-pressed="true">dodaj kurs</a>
-        
-    </div>
-    
+<div class="under_nav">
 
-    <table class="table   text-center mt-3">
-              <thead>
-                <tr>
-                     <th scope="col">Naziv kursa</th>
-                     <th scope="col">Pogledaj kurs </th>
-                     <th scope="col">Izmeni</th>
-                     <th scope="col">Obrisi</th>
-                </tr>
-               </thead>
-                <tbody>
-                <?php  foreach ($courses as $course) { ?>
-                    <tr>
-                        <td><?php echo $course->name; ?>  </td>
-                        <td><a id="pogledaj_lekc" href="single_course_admin?id=<?php echo $course->id;?>"><button class="btn btn-primary btn-block">Pogledaj kurs</button></a></td>
-                        <td><a href="edit_course?id=<?php echo $course->id; ?>"><button class="btn btn-danger ">Izmeni</button> </a></td>
-                        <td><a href="delete_course?id=<?php echo $course->id;  ?>&courseId=<?php echo $course->id;  ?>"><button class="btn btn-warning ">Obrisi</button> </a></td>
+    <p class="under_nav_parag">Admin panel</p>
+
+</div>
 
 
-                    </tr>
-                    <?php } ?>
-                </tbody>
+<a href="addCourses"> <button class="buy_course_button">Dodaj kurs</button> </a>
 
 
+<div class="std_lecture_teacher">
+    <?php  foreach ($courses as $course) { ?>
+    <div class="std_item"><b><?php echo $course->name; ?></b></div>
+    <div class="std_item" id="look"><a href="single_course_admin?id=<?php echo $course->id;?>">Pogledaj kurs</a></div>
+    <div class="std_item" id="edit"><a href="edit_course?id=<?php echo $course->id; ?>">Izmeni</a></div>
+    <div class="std_item" id="delete"><a href="delete_course?id=<?php echo $course->id;  ?>&courseId=<?php echo $course->id;  ?>">Obrisi</a></div>
+
+    <?php } ?>
+</div>
 
 
-             </table>
-
-
- </div>
 
 </body>
 </html>
