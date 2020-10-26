515d6a46a58b9863327eba54fa9da68c836c206e
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-26 15:09:22 +0100

    Update addLecture.view.php

--- app/views/addLecture.view.php
+++ app/views/addLecture.view.php
@@ -1,51 +1,61 @@
 <?php require 'partials/header.php';  ?>
 
-    <div class="container-fluid"> 
-        <div class="row p-2" style='background-color:#4f4d49';> 
-            <div> 
-               <span class="text-white"> dodavanje lekcije </span>
-            </div> 
+<div class="under_nav">
 
-         </div>
+    <p class="under_nav_parag">Dodaj lekciju</p>
 
-         <?php if(isset($_SESSION['errors'])){
-        foreach($_SESSION['errors'] as $er){
-            ?> 
-            <div class="alert alert-danger text-center" role="alert">
-                 <?php echo $er;?>
-            </div>
-            <?php
-        }
-        unset($_SESSION['errors']);       
-    } ?>
-
-         <form action="addLecture_post?course_id=<?php echo $_GET['coursee_id'];?>" method="post"  enctype="multipart/form-data">
-            <div class="form-group">
-                <p class="mt-3"><b>naslov lekcije</b></p>
-                <input type="text" name="name" class="form-control col-xl-4 col-6" id="exampleFormControlInput1">
-             </div>
-            <div class="form-group">
-                <p class="mt-3"><b>  kratak opis  </b></p>
-                <textarea class="form-control col-xl-4 col-8" name="short_description" rows="3"></textarea>
-            </div>
-
-            <div class="form-group">
-                 <p class="mt-3"><b>  sadrzaj  </b></p>
-                 <textarea class="form-control col-xl-6 col-10" name="content"   rows="7"></textarea>
-            </div>
-
-             <div class="form-group">
-                 <p class="mt-3"><b>izaberi fajl  </b></p>
-                  <input type="file" class="form-control-file" name="document" id="exampleFormControlFile1">
-            </div>
-
-            <button type="submit" class="btn btn-primary col-xl-2 col-4 btn-block">Dodaj</button>
+</div>
 
 
 
-         </form>
+<div class="std_exercise_txt">
 
-     </div>
+    <div class="std_exe_txt"></div>
+    <div class="std_exe_txt">
+
+        <?php if(isset($_SESSION['errors'])){
+            foreach($_SESSION['errors'] as $er){
+                ?>
+                <div class="flash_message">
+                    <?php echo $er;?>
+                </div>
+                <?php
+            }
+            unset($_SESSION['errors']);
+        } ?>
+
+
+
+        <form action="addLecture_post?course_id=<?php echo $_GET['coursee_id'];?>" method="post"  enctype="multipart/form-data">
+
+            <p class="form_parag">Naziv lekcije</p>
+            <input class="input_lec" type="text" name="name">
+
+            <p class="form_parag">Sadrzaj lekcije</p>
+            <textarea name="content" class="textarea_std" rows="12" cols="100" ></textarea>
+            <!--
+            <div>
+            <p class="form_parag">Izaberi fajl</p>
+            <input type="file" name="document">
+            </div> -->
+
+
+
+            <button type="submit" class="deliver_exercise_button">Dodaj</button>
+
+        </form>
+
+
+
+
+    </div>
+
+    <div class="std_exe_txt"></div>
+
+
+
+</div>
+
 
 
 </body>
