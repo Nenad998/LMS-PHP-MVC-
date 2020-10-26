b0f499607a57aed70e3657cf930385f76e8d08ad
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-26 14:46:17 +0100

    Update teacher_on_course.view.php

--- app/views/teacher_on_course.view.php
+++ app/views/teacher_on_course.view.php
@@ -1,18 +1,27 @@
 <?php require 'partials/header.php'; ?>
 
-<div class="container-fluid mt-2  h-75 d-inline-block ">
-    <div class="row ">
-        <div class="col-xl-2"></div>
-        <div class="col-xl-8 mt-3"><p><b>Profesor na ovom kursu je:</b></p>
+<style>
+    body {
+        background: #e4e4e4;
+    }
 
-            <p> <?php echo "<b>" . $course->user->name . "</b>"; ?> </p>
+</style>
 
+<div class="form-container">
+
+    <form class="form-wrap">
+
+
+
+        <h1>Profesor na kursu</h1>
+        <div class="form-box">
+
+            <p class="name_prof"><?php echo $course->user->name ?> </p>
 
         </div>
-        <div class="col-xl-2"></div>
-    </div>
-</div>
+    </form>
 
+</div>
 
 </body>
 </html>
