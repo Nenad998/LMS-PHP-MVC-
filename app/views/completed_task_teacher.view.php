edc699e76b236417a34936e9e25e3936b65bee10
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-28 13:50:17 +0100

    Update completed_task_teacher.view.php

--- app/views/completed_task_teacher.view.php
+++ app/views/completed_task_teacher.view.php
@@ -1,34 +1,35 @@
 <?php require 'partials/header.php';  ?>
 
-  <div class="container-fluid mt-2  h-75 d-inline-block ">
-       <div class="row ">
-         <div class="col-xl-2"> </div>
-           <div class="col-xl-8 mt-3">
-           <table class="table   text-center mt-3">
-           <thead>
-                <tr>
-                     <th scope="col">Ovaj zadatak su uradili ucenici</th>
-                     <th scope="col">Datum</th>
-                   
-                </tr>
-               </thead>
-               <tbody>
-                 <?php foreach($users_tasks as $task){ ?>
-                   <tr>
-                   <td> <?php  echo $task->user->name;   ?> </td>
-                   <td> <?php  echo $task->exercise->dates;   ?> </td>
+<div class="under_nav">
+
+    <p class="under_nav_parag">Uradjeni zadaci</p>
+
+</div>
 
 
-                    </tr>
-                  <?php }?>
-               </tbody>
-            </table>
-    
-              <div class="col-xl-2">  </div>
-          </div>
-       </div>
-     </div>
+<table class="table">
+    <thead>
+    <th>Ime ucenika</th>
+    <th>Datum</th>
+    <th>Sadrzaj</th>
+    <th>Dokument</th>
+    </thead>
+    <tbody>
+    <?php foreach($users_tasks as $task){
+    if(is_object($task)){
+    ?>
+    <tr>
+        <td data-label="Ime ucenika"><?php  echo $task->user->name;   ?></td>
+        <td data-label="Datum"><?php  echo $task->exercise->dates;   ?></td>
+        <td data-label="Sadrzaj"><a class="download_parag" href="teacher_content?id=<?php  echo $task->id;  ?>">Pogledaj</a></td>
+        <td data-label="Dokument"><a class="download_parag" target="_blank" href="/<?php echo $task->document;?>">Preuzmi zadatak</a></td>
+    </tr>
+    <?php } } ?>
 
+
+
+    </tbody>
+</table>
  
 </body>
 </html>
