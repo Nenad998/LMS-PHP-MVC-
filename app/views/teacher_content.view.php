013aead87391cce8c57a973a8e6f6af85b773439
Nenad998 <69111430+Nenad998@users.noreply.github.com>
2020-10-28 13:49:35 +0100

    Add files via upload

---
+++ app/views/teacher_content.view.php
@@ -0,0 +1,28 @@
+<?php require 'partials/header.php';  ?>
+
+
+<div class="under_nav">
+
+    <p class="under_nav_parag">Sadrzaj zadatka</p>
+
+</div>
+
+
+
+<div class="std_exercise_txt">
+
+    <div class="std_exe_txt"></div>
+    <div class="std_exe_txt">
+
+        <p class="content_exe_std"><p><?php echo $content->content;  ?></p><br>
+
+
+
+
+    </div>
+
+    <div class="std_exe_txt"></div>
+
+
+
+</div>
